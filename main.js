3adc111fc147261fcac7a3c1baa313ced30b0cd9
Tanishq-Jainop <78198324+Tanishq-Jainop@users.noreply.github.com>
2021-06-08 18:35:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+menu_list_array = ["Veg Margherita Pizza","veg_supreme_pizza","panner_tikka_pizza","deluxe_veggie_pizza"
+                    ];
+
+function getmenu(){
+var htmldata="<ol class='menulist'>";
+menu_list_array.sort()
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>'+menu_list_array[i]+'</li>'
+}
+htmldata=htmldata+'</ol>'
+document.getElementById("display_menu").innerHTML=htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section class='cards'>"
+for(var i=0;i<menu_list_array.length;i++)
+
+
+htmldata=htmldata+'<div class="cards">'
++'<img src="images/pizzaImg.png"/>'
++menu_list_array[i] + '</div>'
+}
+htmk=htmldata+"</section>"
+document.getElementById("display_addmenu").innerHTML=htmldata
+
+function add_top(){
+var item=document.getElementById("add_item").value;
+menu_list_array.push(item);
+add_item();
+}
